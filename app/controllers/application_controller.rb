78d3063a33658fde8a3b052cc8ca1051ad2d16c3
hizqeelrz <hizqeel1214@gmail.com>
2016-09-22 22:18:12 +0500

    redirect_to root after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for resource
-  	# return the path based on resource
+  	root_path(resource)
   end
 
 end
