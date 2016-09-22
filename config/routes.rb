fcf1bf1f2fa2effe5ff3bdde3dcb3253109f58af
hizqeelrz <hizqeel1214@gmail.com>
2016-09-22 22:03:28 +0500

    add dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  root 'dashboards#dashboard'
 
   devise_for :users, path: "auth", path_names: { sign_in: "login", sign_out: "logout" }
 
