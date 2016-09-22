8eb83c88081f22da54c7501416926cff687b84a2
hizqeelrz <hizqeel1214@gmail.com>
2016-09-22 21:37:37 +0500

    add roles, cities, area and config annotation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-	enum role: [:ceo, :account_manager, :production_manager, :sales_manager]
+	enum role: [:ceo, :director_marketing, :managing_director, :marketing_manager, :national_sales_manager, :sales_manager, :area_sales_manager, :sales_officer, :plant_manager, :production_manager, :production_incharge, :quality_control_manager, :quality_control_assistant, :head_accountant, :assistant_accountant, :tablet_technician, :syrup_technician, :tablet_coating_technician, :extract_technician, :cosmatics_technician, :packing_staff]
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
---
+++ config/initializers/constants.rb
@@ -0,0 +1,13 @@
+PROVINCE = [ "punjab", "kpk", "sindh_balochistan" ]
+
+AREA_PUNJAB = [ "lahore", "rawalpindi", "faisalabad", "multan", "dera_ghazi_khan", "rahim_yar_khan", "sahiwal", "gujranwala", "gujrat" ]
+
+AREA_KPK = [ "dera_ismail_khan", "peshawar", "abbatabad", "sawat", "gilgit_baltistan" ]
+
+AREA_SINDH_BALOCHISTAN = [ "hydrabad", "sukkhar", "larkana", "nawabshah", "karachi" ]
+
+PNJAB_CITIES = [ "lahore", "rawalpindi", "faisalabad", "multan", "dera_ismail_khan", "sahiwal", "gujrat", "narowal", "sadiqabad", "attock", "murree", "chakwal", "bahawalpur", "lodhran", "rahim_yar_khan", "taxila", "sargodha", "kasur", "sheikhu_pura", "nankana_sahib", "jhang", "toba_tek_singh", "khoshaab", "chinyot", "khanewal", "muzafar_garh", "vehari", "layyah", "taunsa", "raajan_pur", "mandi_bahauddin", "okara", "pakpattan", "sialkot", "wazirabad", "hafizabad", "gujranwala", "mirpur", "jehlum" ]
+
+KPK_CITIES = [ "bhakkar", "mianwali", "bannu", "lucky_marwat", "baajor_agency","mardan", "noshehra", "kohat", "para_chanaar", "manshera", "batgram", "muzafar_abad", "haripur_hazara", "bunair", "shangla", "taimer_gran", "butkhaila", "upper_dir", "basham", "sakardu", "gilgit" ]
+
+SINDH_BALOCHISTAN_CITIES = [ "hydrabad", "mirpur_khass", "tando_adam", "badin", "sukkar", "khair_pur", "ghotki", "khasmor", "larkana", "dadu", "jacob_abad", "nawabshah", "noshehro_feroz", "saanhgar", "karachi_city1", "karachi_city2", "karachi_city3" ]
