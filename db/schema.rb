31aea61e415564e5c4c254b32f70fa42c3647aec
hizqeelrz <hizqeel1214@gmail.com>
2016-09-24 03:23:40 +0500

    add role for user and dashboard for ceo in views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918183802) do
+ActiveRecord::Schema.define(version: 20160923220539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,10 +97,12 @@ ActiveRecord::Schema.define(version: 20160918183802) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["role"], name: "index_users_on_role", using: :btree
 
   add_foreign_key "assets", "asset_categories"
   add_foreign_key "products", "product_categories"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: "user1@medix.com", password: "password", password_confirmation: "password")
+User.delete_all
+User.create(email: "user1@medix.com", password: "password", password_confirmation: "password", role: "ceo")
