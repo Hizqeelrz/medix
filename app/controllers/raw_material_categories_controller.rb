1be53bc9ecfb9d2b7e269dd8a5ebc01f9ed7fed3
hizqeelrz <hizqeel1214@gmail.com>
2016-10-14 11:09:27 +0500

    raw_material & category added

--- app/controllers/raw_material_categories_controller.rb
+++ app/controllers/raw_material_categories_controller.rb
@@ -1,13 +1,50 @@
 class RawMaterialCategoriesController < ApplicationController
+
   def index
+  	@raw_material_categories = RawMaterialCategory.all
   end
 
   def new
+  	@raw_material_category = RawMaterialCategory.new
+  end
+
+  def create
+  	@raw_material_category = RawMaterialCategory.new(category_params)
+
+  	if @raw_material_category.save
+  		redirect_to raw_material_categories_path
+  	else
+  		render :new
+  	end
   end
 
   def show
+  	@raw_material_category = RawMaterialCategory.find(params[:id])
   end
 
   def edit
+  	@raw_material_category = RawMaterialCategory.find(params[:id])
+  end
+
+  def update
+  	@raw_material_category = RawMaterialCategory.find(params[:id])
+
+  	if @raw_material_category.update(category_params)
+  		redirect_to raw_material_categories_path
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@raw_material_category = RawMaterialCategory.find(params[:id])
+  	@raw_material_category.destroy
+  	redirect_to raw_material_categories_path
+  end
+
+  private
+
+  def category_params
+  	params.require(:raw_material_category).permit(:name, :description)
   end
 end
--- app/controllers/raw_materials_controller.rb
+++ app/controllers/raw_materials_controller.rb
@@ -1,13 +1,50 @@
 class RawMaterialsController < ApplicationController
+
   def index
+  	@raw_materials = RawMaterial.all
   end
 
   def new
+  	@raw_material = RawMaterial.new
+  end
+
+  def create
+  	@raw_material = RawMaterial.new(material_params)
+
+  	if @raw_material.save
+  		redirect_to raw_materials_path
+  	else
+  		render :new
+  	end
   end
 
   def show
+  	@raw_material = RawMaterial.find(params[:id])
   end
 
   def edit
+  	@raw_material = RawMaterial.find(params[:id])
+  end
+
+  def update
+  	@raw_material = RawMaterial.find(params[:id])
+
+  	if @raw_material.update(material_params)
+  		redirect_to raw_materials_path
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@raw_material = RawMaterial.find(params[:id])
+  	@raw_material.destroy
+  	redirect_to raw_materials_path
+  end
+
+  private
+
+  def material_params
+  	params.require(:raw_material).permit(:name, :quantity, :price)
   end
 end
