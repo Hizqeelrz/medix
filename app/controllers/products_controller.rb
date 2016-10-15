e99dae6b466ba8293fed32bfafe8fda75cd54acd
hizqeelrz <hizqeel1214@gmail.com>
2016-10-15 18:38:21 +0500

    update products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,33 @@
 class ProductsController < ApplicationController
+
   def index
+  	@products = Product.all
   end
 
   def new
+  	@product = Product.new
+  end
+
+  def create
+  	@product = Product.new(product_params)
+
+  	if @product.save
+  		redirect_to products_path
+  	else
+  		render :new
+  	end
   end
 
   def show
+  	@product = Product.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def product_params
+  	params.require(:product).permit(:name)
   end
 end
