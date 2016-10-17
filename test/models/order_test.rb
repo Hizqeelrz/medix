61bfdc9499a0eb01f9fd431c229fe6d4c622c47a
hizqeelrz <hizqeel1214@gmail.com>
2016-10-18 00:04:56 +0500

    user_id added to orders

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -6,14 +6,17 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  client_id  :integer
+#  user_id    :integer
 #
 # Indexes
 #
 #  index_orders_on_client_id  (client_id)
+#  index_orders_on_user_id    (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_5c8e53c896  (client_id => clients.id)
+#  fk_rails_f868b47f6a  (user_id => users.id)
 #
 
 require 'test_helper'
