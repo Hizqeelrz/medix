d32f30119eec6692e3d4abbf1397c9350f362ccc
hizqeelrz <hizqeel1214@gmail.com>
2016-10-26 12:24:25 +0500

    Google marker added to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -27,7 +27,7 @@ class Order < ActiveRecord::Base
 	belongs_to :client
 	belongs_to :user
 
-	has_many :order_products
+	has_many :order_products, dependent: :delete_all
 	has_many :products, through: :order_products
 
 	accepts_nested_attributes_for :order_products, reject_if: :all_blank, allow_destroy: true
