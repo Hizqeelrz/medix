9bcb6285862269f59b928f7c500ff4ff04e5eefc
hizqeelrz <hizqeel1214@gmail.com>
2016-10-29 23:10:36 +0500

    search action added in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all.order(id: :asc).page(params[:page]).per(20)
+    @users = User.all.order(created_at: :asc)
+    @provinces = Province.all
+    @areas = Area.all
+    @cities = City.all
+    @users = search(@users).page(params[:page]).per(30)
   end
 
   def new
@@ -56,5 +60,26 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :gender, :dob, :nic, :address, :city, :state, :phone, :mobile, :joining_date, :marital_date, :basic_salary, :qualification, :email, :password, :password_confirmation, :role, :job_province, :job_area, :job_city, :boss_id)
   end
-  
+
+  def search scope
+    scope = scope
+    if params[:query].presence
+      scope = scope.search_by_name_phone(params[:query])
+    end
+    if params[:role].presence
+      scope = scope.where(role: params[:role])
+    end
+    if params[:province_id].presence
+      scope = scope.where(province_id: params[:province_id])
+    end
+    if params[:area_id].presence
+      scope = scope.where(area_id: params[:area_id])
+    end
+    if params[:city_id].presence
+      scope = scope.where(city_id: params[:city_id])
+    end
+    scope
+  end
+
+
 end
